d7d5c4980e555cd4e6a516572b2102c1ac498b26
aliya <alexdavydov92@gmail.com>
2023-12-12 22:48:16 +0300

    продвижение? есть!

--- qa_guru_9_10/pages/registration_page.py
+++ qa_guru_9_10/pages/registration_page.py
@@ -1,3 +1,5 @@
+import os
+
 from selene import browser, have, be, by
 from selene.support.shared.jquery_style import s
 
@@ -6,6 +8,18 @@ class RegistrationPage:
     """Открываем страницу и выполняем проверку"""
 
     def __init__(self):
+        self.first_name = s('#firstName')
+        self.last_name = s('#lastName')
+        self.state = s('#react-select-3-input')
+        self.email = s('#userEmail')
+        self.gender = s('#gender-radio-1')
+        self.user_number = s('#userNumber')
+        self.subjects = s('#subjectsInput')
+        self.hobbies = s('[for="hobbies-checkbox-2"]')
+        self.picture = s('#uploadPicture')
+        self.address = s('#currentAddress')
+        self.city = s('#react-select-4-input')
+        self.submit = s('#submit')
         self.should_registered_user_with = s('.table-responsive')
 
     def open(self):
@@ -15,16 +29,46 @@ class RegistrationPage:
     """Заполняем Name"""
 
     def fill_first_name(self, value):
-        return s('#firstName').should(be.blank).type(value)
+        self.first_name.should(be.blank).type(value)
 
     def fill_last_name(self, value):
-        return s('#lastName').should(be.blank).type(value)
+        self.last_name.should(be.blank).type(value)
 
     def fill_email(self, value):
-        return s('#userEmail').should(be.blank).type(value)
+        self.email.should(be.blank).type(value)
+
+    def fill_gender(self):
+        self.gender.double_click()
+
+    def fill_user_number(self, number):
+        self.user_number.type(number)
 
     def fill_birthday(self, year, month, day):
         s('#dateOfBirthInput').click()
         s('.react-datepicker__month-select').click().element(by.text(month)).click()
         s('.react-datepicker__year-select').click().element(by.text(year)).click()
         s(f'.react-datepicker__day--0{day}').click()
+
+    def fill_subjects(self, subjects_name):
+        self.subjects.should(be.blank).type(subjects_name).press_enter()
+
+    def fill_hobbies(self):
+        self.hobbies.click()
+
+    def fill_picture(self, picture_path):
+        self.picture.send_keys(os.path.abspath(picture_path))
+
+    def fill_address(self, adress):
+        self.address.should(be.blank).type(adress)
+
+    def fill_state(self, state_name):
+        self.state.type(state_name).press_enter()
+
+    def fill_city(self, city_name):
+        self.city.type(city_name).press_enter()
+
+    def fill_submit(self):
+        self.submit.press_enter()
+
+    def get_should_registered_user_with(self):
+        return self.should_registered_user_with
--- tests/test_form.py
+++ tests/test_form.py
@@ -1,7 +1,6 @@
 import os.path
-from selene import have, be
+from selene import have, be, by
 from selene.support.shared.jquery_style import s
-
 from qa_guru_9_10.pages.registration_page import RegistrationPage
 
 
@@ -18,37 +17,37 @@ def test_student_registration_form():
     registration_page.fill_email('AlexDavydov92@gmail.com')
 
     """Заполняем Gender"""
-    s('#gender-radio-1').double_click()
+    registration_page.fill_gender()
 
     """Заполняем Mobile"""
-    s('#userNumber').type('8005553535')
+    registration_page.fill_user_number('8005553535')
 
     """Заполняем Date of Birth"""
     registration_page.fill_birthday("1992", "June", "20")
 
     """Заполняем Subjects"""
-    s('#subjectsInput').should(be.blank).type('English').press_enter()
+    registration_page.fill_subjects('English')
 
     """Заполняем Hobbies"""
-    s('[for="hobbies-checkbox-2"]').click()
+    registration_page.fill_hobbies()
 
     """Подгружаем Picture"""
-    s('#uploadPicture').send_keys(os.path.abspath('image/selfies.jpeg'))
+    registration_page.fill_picture('image/selfies.jpeg')
 
     """Вводим Address"""
-    s('#currentAddress').should(be.blank).type('South Street')
+    registration_page.fill_address('South Street')
 
     """Выбираем State """
-    s('#react-select-3-input').type('Haryana').press_enter()
+    registration_page.fill_state('Haryana')
 
     """Выбираем  City"""
-    s('#react-select-4-input').type('Karnal').press_enter()
+    registration_page.fill_city('Karnal')
 
     """Нажимаем Отправить"""
-    s('#submit').press_enter()
+    registration_page.fill_submit()
 
     """Выполняем проверки что форма отправилась и заполнены все поля"""
-    registration_page.should_registered_user_with().should(
+    registration_page.get_should_registered_user_with().should(
         have.text(
             'Alex Davydov'
             and 'AlexDavydov92@gmail.com'
